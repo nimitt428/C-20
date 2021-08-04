43da0230eee28eb613fb13d074e42d6031e5930e
nimitt428 <86357905+nimitt428@users.noreply.github.com>
2021-08-04 16:41:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+var bath;
+var gym;
+var eat;
+var Move;
+var sleep;
+var bg , space ;
+var Astronaut;
+
+
+
+
+function preload(){
+bg = loadImage("iss.png")
+bath = loadAnimation("bath1.png","bath2.png")
+gym = loadAnimation("gym1.png","gym2.png","gym11.png","gym12.png")
+eat= loadAnimation("eat1.png","eat2.png")
+sleep = loadAnimation("sleep.png")
+Move = loadAnimation("sleep.png")
+brush = loadAnimation("brush.png")
+}
+
+function setup() {
+  createCanvas(400,400);
+  
+  space = createSprite(200,200,400,400)
+  space.addAnimation("space",bg)
+  space.scale = 0.1;
+  text("Press M and Arrows to see Astronaut daily routine",100,50)
+  
+  
+  Astronaut = createSprite(200, 250);
+  Astronaut.addAnimation("Astronaut",Move)
+  Astronaut.scale = 0.1;
+  text(text,100,70);
+ 
+   
+}
+
+function draw() {
+  background(255,255,255); 
+  
+  createEdgeSprites();
+  //Astronaut.bounceOff(Edges);
+  
+  text("Press M and Arrows to see Astronaut daily routine",100,50)
+  
+  if(keyDown("UP_ARROW")){
+  Astronaut.addAnimation("brushing",brush)
+  Astronaut.changeAnimation("brushing")
+  Astronaut.y = 250;
+  Astronaut.VelocityX = 0;
+  Astronaut.VelocityY = 0;
+
+  }
+  if(keyDown("DOWN_ARROW")){
+    Astronaut.addAnimation("gymming",gym)
+    Astronaut.changeAnimation("gymming")
+    Astronaut.y = 250;
+    Astronaut.VelocityX = 0;
+    Astronaut.VelocityY = 0;
+   
+    }
+  
+    if(keyDown("RIGHT_ARROW")){
+    Astronaut.addAnimation("bathing",bath)
+    Astronaut.changeAnimation("bathing")
+    Astronaut.y = 250;
+     Astronaut.VelocityX = 0;
+    Astronaut.VelocityY = 0;  }
+  
+    if(keyDown("LEFT_ARROW")){
+      Astronaut.addAnimation("eating",eat)
+      Astronaut.changeAnimation("eating")
+      Astronaut.y = 250;
+      Astronaut.VelocityX = 1;
+      Astronaut.VelocityY = 1;
+     }
+     
+     if(keyDown("M")){
+      Astronaut.addAnimation("moving",Move)
+      Astronaut.changeAnimation("moving")
+      Astronaut.y = 250;
+      Astronaut.VelocityX = 2;
+      Astronaut.VelocityY = 2;
+     }
+              
+  
+  
+  drawSprites();
+}
